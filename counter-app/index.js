6a1dc5d62a6d69289282e5fffb4a0b228b3c30ef
Konstantin Teryohin <konstantin.teryohin@rubius.com>
2022-11-08 22:13:27 +0700

    cleanup

--- counter-app/index.js
+++ counter-app/index.js
@@ -17,15 +17,10 @@ const redisClient = createClient({ url: REDIS_URL });
     counterApp
         .post('/counter/:bookId/incr', async (req, res) => {
             const { bookId } = req.params;
-            console.log('[debug] POST', bookId);
 
             const count = await redisClient.get(bookId);
-            console.log('[debug] count', count);
             const _nextCount = count ? parseInt(count, 0) + 1 : 1;
-            console.log('[debug] _nextCount', _nextCount);
-
             await redisClient.set(bookId, _nextCount);
-            console.log('[debug] set');
 
             res.json({
                 bookId: req.params.bookId,
@@ -34,11 +29,7 @@ const redisClient = createClient({ url: REDIS_URL });
         })
         .get('/counter/:bookId', async (req, res) => {
             const { bookId } = req.params;
-            console.log('[debug] GET', bookId);
             const count = await redisClient.get(bookId);
-            console.log('[debug] count', count);
-            console.log('[debug] resp', { count: count ? JSON.parse(count, 0) : 0 });
-
             res.json({ count: count ? JSON.parse(count, 0) : 0 });
         });
 
